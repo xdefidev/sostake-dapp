9594d9a25d214deefcd001b4c39a5e4cc0a18932
xdefidev <103839000+xdefidev@users.noreply.github.com>
2022-04-27 00:35:09 +0100

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -1,12 +1,12 @@
 "use strict"
-const tokenAddress = "0x0b399e87add76f6890fa28deb624cbee6a2dfe21";
+const tokenAddress = "0x0F2B5826C88E2321EAeB2A6F79063e7DFf37329A";
 const wbnb = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
 const treasuryAddress = '0x9432f7c5370876Df5aF4397cDaFBDc50fE33FB11';
-const pairAddress = '0xdB969A3aF52a3754D1Db3D4acE22FEF3883D9353';
+const pairAddress = '0xDc93180c79DCdE0a901De469E8Fcb0D29b72f235';
 // const usdc = '0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664';
 
 const apiKey = "NIRJMYPQHC9S5KA57GFWK4Z86H27CMDN57";
-
+const apiKey2 ="Z5ICY43DWRMVB12642VWQDVVF5IP36FFR6"
 const decimals = 18;
 
 
@@ -15,7 +15,8 @@ const numberFormat = new Intl.NumberFormat('en-US', options);
 
 
 let reqHeader = new Headers();
-reqHeader.append('Content-Type', 'text/json');
+// reqHeader.append('Content-Type', 'application/javascript');
+// reqHeader.append('Allow-Control-Allow-Origin', '*');
 
 let initObject = {
     method: 'GET',
@@ -27,7 +28,7 @@ Promise.all([
         fetch("https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress="+ wbnb + "&address=" + pairAddress +"&tag=latest&apikey="+ apiKey, initObject),
         fetch("https://api.bscscan.com/api?module=account&action=balance&address=" + treasuryAddress +"&tag=latest&apikey="+ apiKey, initObject),
         fetch("https://api.pancakeswap.info/api/v2/tokens/" + wbnb, initObject),
-        fetch("https://api.bscscan.com/api?module=stats&action=tokenCsupply&contractaddress=" + tokenAddress + "&apikey" + apiKey, initObject)
+        fetch("https://api.bscscan.com/api?module=stats&action=tokenCsupply&contractaddress=" + tokenAddress + "&apikey" + apiKey2, initObject)
 
     ]).then( function (responses) {
         return Promise.all(responses.map(function (response) {
@@ -35,10 +36,10 @@ Promise.all([
         }));
     }).then( function (data){
         
-        let price = data[0].price;
+        let price = data[0].data.price;
         let pairBalance = data[1].result;
         let treasury = data[2].result;
-        let bnbPrice = data[3].price;
+        let bnbPrice = data[3].data.price;
         let supply = data[4].result;
 
         console.log(bnbPrice);
@@ -51,7 +52,7 @@ Promise.all([
        
         document.getElementById("treasury").innerHTML = numberFormat.format(((treasury / Math.pow(10, decimals)) * bnbPrice).toFixed(2));
             document.getElementById("pool").innerHTML = "$" + ((pairBalance / Math.pow(10, decimals)) * bnbPrice).toFixed(2);
-            document.getElementById("dropbtn").innerHTML = "HYBRID " + "$" + Number(price).toFixed(3);
+            document.getElementById("dropbtn").innerHTML = "SOS " + "$" + Number(price).toFixed(3);
             document.getElementById("price").innerHTML = "$" + Number(price).toFixed(3);
             document.getElementById("price2").innerHTML = "$" + Number(price).toFixed(3);
             document.getElementById("price3").innerHTML = "$" + Number(price).toFixed(3);
